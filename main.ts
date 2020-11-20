6af4b93ac687dc2b70ed2b243944742df864190f
warman7526 <74411847+warman7526@users.noreply.github.com>
2020-11-20 13:02:23 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,40 @@
-
+function move_pet () {
+    playerPet.setPosition(playerSprite.x + 10, playerSprite.y + 10)
+}
+function update () {
+    move_pet()
+}
+let playerSprite: Sprite = null
+let playerPet: Sprite = null
+playerPet = sprites.create(img`
+    . 7 7 7 7 7 7 7 . 
+    7 7 7 7 7 7 7 7 7 
+    7 f f 7 7 f f 7 7 
+    7 f 1 7 7 f 1 7 7 
+    7 7 7 7 7 7 7 7 7 
+    7 7 f f f f f f 7 
+    7 7 7 f 2 2 2 f 7 
+    7 7 7 7 7 7 7 7 7 
+    . 7 7 7 7 7 7 7 . 
+    `, SpriteKind.Player)
+playerSprite = sprites.create(img`
+    . . . . . . . . . . . . . . . 
+    . . . f f f f f f f f f . . . 
+    . . f 8 1 8 8 8 8 8 1 8 f . . 
+    . . f 8 1 8 8 8 8 8 1 8 f . . 
+    . . . f f f f f f f f f . . . 
+    . . . . f 6 6 6 6 6 f . . . . 
+    . . f f f f f f f f f f f . . 
+    . f f 9 9 9 9 9 9 9 9 9 f f . 
+    . f 9 9 9 9 9 9 9 9 9 9 9 f . 
+    . f 9 9 9 9 9 9 9 9 9 9 9 f . 
+    . f 9 9 9 9 9 9 9 9 9 9 9 f . 
+    . f f 9 9 9 9 9 9 9 9 9 f f . 
+    . . f f f f f f f f f f f . . 
+    . . . f 1 1 f . f 1 1 f . . . 
+    . . . . f f . . . f f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(playerSprite)
+forever(function () {
+    update()
+})
